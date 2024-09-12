c26ae2749585f0d99e890ef809f8702cbeebfd9c
Veikka, Liljander <ad1542@student.jamk.fi>
2024-09-12 14:38:08 +0300

    mineraali jutut

--- app/(tabs)/minerals.tsx
+++ app/(tabs)/minerals.tsx
@@ -1,40 +1,91 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView, Text } from 'react-native';
+import MineralButton from '@/components/MineralButton';
 import Teksti from '@/components/Textbox';
 
-const Minerals: React.FC = () => {
+type MineralData = {
+  id: string;
+  image: any;
+};
+
+const data: MineralData[] = [
+  {
+    id: 'asdfasdf',
+    image: require('../../assets/images/placeholdermineral.jpg'),
+  },
+  {
+    id: 'fhjnhjkrtnhj',
+    image: require('../../assets/images/placeholdermineral.jpg'),
+  },
+  {
+    id: 'Pisces',
+    image: require('../../assets/images/placeholdermineral.jpg'),
+  },
+  { id: 'Aries', image: require('../../assets/images/placeholdermineral.jpg') },
+  {
+    id: 'Taurus',
+    image: require('../../assets/images/placeholdermineral.jpg'),
+  },
+  {
+    id: 'Gemini',
+    image: require('../../assets/images/placeholdermineral.jpg'),
+  },
+  {
+    id: 'Cancer',
+    image: require('../../assets/images/placeholdermineral.jpg'),
+  },
+  { id: 'Leo', image: require('../../assets/images/placeholdermineral.jpg') },
+  { id: 'Virgo', image: require('../../assets/images/placeholdermineral.jpg') },
+];
+
+function Minerals({ navigation }: { navigation: any }) {
   return (
-    <View style={styles.container}>
-      <Teksti>
-        <Text style={styles.regularText}>
-          Täällä on mineraaleja kadwa regular
-        </Text>
-        <Text style={styles.boldText}>Täällä on mineraaleja kadwa bold</Text>
-        <Text>Täällä on mineraaleja normaali</Text>
+    <View style={styles.background}>
+      <Text style={styles.header}>FIND a suitable crystal for your mood</Text>
+      <Teksti style={styles.stonebox}>
+        <ScrollView contentContainerStyle={styles.grid}>
+          {data.map((item) => (
+            <MineralButton
+              key={item.id}
+              title={item.id}
+              img={item.image}
+              onPress={() => navigation.navigate('Horoscope')}
+            />
+          ))}
+        </ScrollView>
       </Teksti>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
     backgroundColor: '#3F3154',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  regularText: {
-    fontFamily: 'Kadwa_400Regular',
-    fontSize: 20,
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: 10,
   },
-  boldText: {
+  stonebox: {
+    backgroundColor: '#918998',
+    borderColor: '#ACA3AF',
+    borderWidth: 4,
+    height: '60%',
+    width: '80%',
+  },
+  header: {
+    fontSize: 30,
     fontFamily: 'Kadwa_700Bold',
-    fontSize: 20,
+    color: 'white',
+    marginHorizontal: 40,
+    height: '20%',
   },
 });
+
 export default Minerals;
---
+++ components/MineralButton.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Pressable, Text, Image, View } from 'react-native';
+
+interface MineralButtonProps {
+  title: string;
+  img?: any;
+  onPress: () => void;
+}
+
+const MineralButton: React.FC<MineralButtonProps> = ({
+  title,
+  img,
+  onPress,
+}) => {
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <View style={styles.content}>
+        <Image source={img} style={styles.image}></Image>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+};
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 35,
+    width: '30%',
+    height: 100,
+    marginVertical: 10,
+    borderColor: 'red',
+    borderWidth: 2,
+    fontFamily: 'Kadwa_400Regular',
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    fontFamily: 'Kadwa_400Regular',
+    textAlign: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  content: {
+    alignItems: 'center',
+  },
+});
+export default MineralButton;
