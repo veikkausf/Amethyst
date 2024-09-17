59c840e2a554286f66714e8305dce8f464c01f97
Veikka, Liljander <ad1542@student.jamk.fi>
2024-09-17 13:55:33 +0300

    mineraldata juttu

--- app/(tabs)/mineral-data.tsx
+++ app/(tabs)/mineral-data.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
 
 // Parametri-lista tälle komponentille
@@ -18,22 +18,55 @@ const MineralData: React.FC<Props> = ({ route }) => {
 
   return (
     <View style={styles.background}>
-      <Text>Mineral ID: {itemId}</Text>
+      <Image
+        style={styles.image}
+        source={require('../../assets/images/mineral_icon.png')}
+      ></Image>
+      <Text style={styles.header}>{itemId}</Text>
+      <Text style={styles.headerLeft}>Shackra: sajajajajajajajja</Text>
+      <Text style={styles.headerRight}>
+        Horoscope:
+        <Text style={styles.normalFont}> Libra, Sagittarius, Capricorn </Text>
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
     backgroundColor: '#3F3154',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 30,
+    fontFamily: 'Kadwa_700Bold',
+    color: 'white',
+    marginHorizontal: 40,
+    height: '20%',
+  },
+  headerLeft: {
+    fontSize: 20,
+    fontFamily: 'Kadwa_700Bold',
+    color: 'white',
+
+    height: '20%',
+  },
+  headerRight: {
+    fontSize: 20,
+    fontFamily: 'Kadwa_700Bold',
+    color: 'white',
+    textAlign: 'right',
+    left: 60,
+    width: '60%',
+  },
+  image: {
+    height: '10%',
+    width: '10%',
+  },
+  normalFont: {
+    fontFamily: 'Kadwa_400Regular',
   },
 });
 
