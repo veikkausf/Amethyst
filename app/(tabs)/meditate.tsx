34507e1118117fc7c3503ebb9e5dd9753c3568eb
Veikka, Liljander <ad1542@student.jamk.fi>
2024-09-20 09:53:17 +0300

    meditaatio

--- app/(tabs)/meditate.tsx
+++ app/(tabs)/meditate.tsx
@@ -1,29 +1,96 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Teksti from '@/components/Textbox';
+import MedButton from '@/components/MedButton';
 
-const Meditate: React.FC = () => {
+type data = {
+  id: string;
+  image: any;
+};
+
+const mediData: data[] = [
+  {
+    id: 'Morning brew',
+    image: require('../../assets/images/meditationpc.png'),
+  },
+  {
+    id: 'Anxiety help',
+    image: require('../../assets/images/meditationpc.png'),
+  },
+  {
+    id: 'Better sleep',
+    image: require('../../assets/images/meditationpc.png'),
+  },
+];
+
+const natData: data[] = [
+  { id: 'Nightfall', image: require('../../assets/images/meditationpc.png') },
+  {
+    id: 'Windy field',
+    image: require('../../assets/images/meditationpc.png'),
+  },
+  {
+    id: 'Fireplace',
+    image: require('../../assets/images/meditationpc.png'),
+  },
+];
+
+function Meditate({ navigation }: { navigation: any }) {
   return (
-    <View style={styles.container}>
-      <Teksti>
-        <Text> Täällä Voit meditoida</Text>
+    <View style={styles.background}>
+      <Text style={styles.header}>
+        Select
+        <Text style={styles.normalFont}>Your serenity</Text>
+      </Text>
+      <Teksti style={styles.stonebox}>
+        <ScrollView horizontal={true}>
+          {mediData.map((item) => (
+            <MedButton
+              key={item.id} //Napin avain jokaiselle napille listasta
+              title={item.id} //Napin otsikko teksti
+              img={item.image} //Napin kuva
+              onPress={
+                () => navigation.navigate('MineralData', { itemId: item.id }) //Navigoidaan "MineralData" sivulle ja viedään sinne samalla ID parametrinä
+              }
+            />
+          ))}
+        </ScrollView>
       </Teksti>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
     backgroundColor: '#3F3154',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: 10,
+  },
+  stonebox: {
+    backgroundColor: '#918998',
+    borderColor: '#ACA3AF',
+    borderWidth: 4,
+    height: '65%',
+    width: '90%',
+    margin: 15,
+  },
+  header: {
+    fontSize: 30,
+    fontFamily: 'Kadwa_700Bold',
+    color: 'white',
+    marginHorizontal: 40,
+    height: '20%',
+  },
+  normalFont: {
+    fontFamily: 'Kadwa_400Regular',
+  },
 });
-
 export default Meditate;
---
+++ components/MedButton.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, Pressable, Text, Image, View } from 'react-native';
+
+interface MedButtonProps {
+  title: string;
+  img?: any;
+  onPress: () => void;
+}
+
+const MedButton: React.FC<MedButtonProps> = ({ title, img, onPress }) => {
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <View style={styles.content}>
+        <Image source={img} style={styles.image}></Image>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+};
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 35,
+    width: '33%',
+    height: 100,
+    marginVertical: 10,
+
+    fontFamily: 'Kadwa_400Regular',
+  },
+  text: {
+    color: 'black',
+    fontSize: 12,
+    fontFamily: 'Kadwa_400Regular',
+    textAlign: 'center',
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+  },
+  content: {
+    alignItems: 'center',
+  },
+});
+export default MedButton;
