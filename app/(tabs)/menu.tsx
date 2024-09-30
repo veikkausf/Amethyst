b15333d3b08234f3160f31e66fe5975f3c5075ba
ad1770 <ad1770@student.jamk.fi>
2024-09-30 16:13:55 +0300

    toimiva responsiivinen menu

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,47 +1,107 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import MenuButton from '@/components/MenuButton';
+import {
+  StyleSheet,
+  Pressable,
+  Text,
+  ViewStyle,
+  Image,
+  View,
+} from 'react-native';
 
-interface HomeScreenProps {
+import Teksti from '../../components/Textbox';
+
+interface MenuButtonProps {
+  title: string; //Otsikko
+  text: string; //Sisältö
+  onPress: () => void;
+  img?: any; //Kuva
   navigation: any;
 }
+
 //Menu painikkeet kutsumalla MenuButton komponenttia
 //onPress() funtio navigointia varten
-function MenuScreen({ navigation }: HomeScreenProps) {
+function MenuScreen({ navigation }: MenuButtonProps) {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>
         <Text style={styles.headerbold}>Choose</Text> a way to calm your mind...
       </Text>
-      <MenuButton
-        title="Daily Tarot                "
-        text="Get your daily guidance"
+      {/* Tarot  */}
+      <Pressable
+        style={styles.containerLeft}
         onPress={() => navigation.navigate('Tarot')}
-        style={styles.right}
-        img={require('../../assets/images/menu-tarot.png')}
-      />
-
-      <MenuButton
-        title="Today's horoscope"
-        text="Whats in the air today"
+      >
+        <Teksti style={styles.textBoxLeft}>
+          <View style={styles.borderLeft}>
+            <Image
+              style={styles.imageleft}
+              source={require('../../assets/images/menu-tarot.png')}
+            />
+          </View>
+          <View>
+            <Text style={styles.headerLeft}>Daily tarot</Text>
+            <Text style={styles.textLeft}>Get your daily guidance</Text>
+          </View>
+        </Teksti>
+      </Pressable>
+      {/* Horoscope */}
+      <Pressable
+        style={styles.containerRight}
         onPress={() => navigation.navigate('Horoscope')}
-        style={styles.left}
-        img={require('../../assets/images/menu-horo.png')}
-      />
-      <MenuButton
-        title="Meditation lessons"
-        text="Clear your mind and relax"
+      >
+        <Teksti style={styles.textBoxRight}>
+          <View style={styles.borderRight}>
+            <Image
+              style={styles.imageright}
+              source={require('../../assets/images/menu-horo.png')}
+            />
+          </View>
+          <View>
+            <Text style={styles.headerRight}>Today's horoscope</Text>
+            <Text style={styles.textRight}>What's in the air today</Text>
+          </View>
+        </Teksti>
+      </Pressable>
+
+      {/* Meditation */}
+
+      <Pressable
+        style={styles.containerLeft}
         onPress={() => navigation.navigate('Meditate')}
-        style={styles.right}
-        img={require('../../assets/images/menu-meditation.png')}
-      />
-      <MenuButton
-        title="Crystal guide"
-        text="Learn about crystal energies"
+      >
+        <Teksti style={styles.textBoxLeft}>
+          <View style={styles.borderLeft}>
+            <Image
+              style={styles.imageleft}
+              source={require('../../assets/images/menu-meditation.png')}
+            />
+          </View>
+          <View>
+            <Text style={styles.headerLeft}>Meditation lessons</Text>
+            <Text style={styles.textLeft}>Clear your mind and relax</Text>
+          </View>
+        </Teksti>
+      </Pressable>
+
+      {/* Crystals  */}
+
+      <Pressable
+        style={styles.containerRight}
         onPress={() => navigation.navigate('Minerals')}
-        style={styles.left}
-        img={require('../../assets/images/menu-mineral.png')}
-      />
+      >
+        <Teksti style={styles.textBoxRight}>
+          <View style={styles.borderRight}>
+            <Image
+              style={styles.imageright}
+              source={require('../../assets/images/menu-mineral.png')}
+            />
+          </View>
+          <View>
+            <Text style={styles.headerRight}>Crystal guide</Text>
+            <Text style={styles.textRight}>Lear about crystal energies</Text>
+          </View>
+        </Teksti>
+      </Pressable>
     </View>
   );
 }
@@ -49,11 +109,8 @@ function MenuScreen({ navigation }: HomeScreenProps) {
 export default MenuScreen;
 //tyylit
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+  // yleiset
+
   container: {
     backgroundColor: '#3F3154',
     flex: 1,
@@ -61,22 +118,112 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
   },
-  left: {
-    justifyContent: 'flex-start',
-    left: 40,
-  },
-  right: {
-    justifyContent: 'flex-start',
-    right: 40,
-  },
+
   header: {
     color: '#ffffff',
     fontSize: 30,
     fontFamily: 'Kadwa_400Regular',
+    marginHorizontal: 15,
   },
   headerbold: {
     color: '#ffffff',
     fontSize: 30,
     fontFamily: 'Kadwa_700Bold',
+  },
+  //
+  // vasemmat
+  //
+  containerLeft: {
+    alignSelf: 'flex-start',
+    alignItems: 'flex-start',
+    marginHorizontal: 10,
+    width: '80%',
+    height: '15%',
+    marginVertical: 30,
+  },
+  textBoxLeft: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'flex-start',
+  },
+
+  imageleft: {
+    width: 60,
+    height: 60,
+    borderColor: '#ACA3AF',
+    borderWidth: 3,
+    borderRadius: 30,
+  },
+  borderLeft: {
+    width: 70,
+    height: 70,
+    borderColor: '#3F3154',
+    borderWidth: 5,
+    borderRadius: 40,
+    left: '103%',
+    position: 'absolute',
+    top: '30%',
+  },
+  headerLeft: {
+    textAlign: 'left',
+    color: 'white',
+    fontFamily: 'Kadwa_400Regular',
+    fontSize: 25,
+  },
+
+  textLeft: {
+    textAlign: 'left',
+    color: 'white',
+    fontFamily: 'Kadwa_400Regular',
+    fontSize: 15,
+  },
+
+  //
+  // oikeat
+  //
+  containerRight: {
+    alignSelf: 'flex-end',
+    alignItems: 'flex-end',
+    marginHorizontal: 10,
+    width: '80%',
+    height: '15%',
+  },
+  imageright: {
+    width: 60,
+    height: 60,
+    borderColor: '#ACA3AF',
+    borderWidth: 3,
+    borderRadius: 30,
+  },
+
+  borderRight: {
+    width: 70,
+    height: 70,
+    borderColor: '#3F3154',
+    borderWidth: 5,
+    borderRadius: 40,
+    right: '103%',
+    position: 'absolute',
+    top: '30%',
+  },
+
+  textBoxRight: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'flex-end',
+  },
+
+  headerRight: {
+    textAlign: 'right',
+    color: 'white',
+    fontFamily: 'Kadwa_400Regular',
+    fontSize: 25,
+  },
+
+  textRight: {
+    textAlign: 'right',
+    color: 'white',
+    fontFamily: 'Kadwa_400Regular',
+    fontSize: 15,
   },
 });
