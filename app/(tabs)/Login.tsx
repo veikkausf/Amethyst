344ba757d1063f2c1f38df47daf918c9f530e816
Veikka, Liljander <ad1542@student.jamk.fi>
2024-11-14 14:14:39 +0200

    birthdaymodal kesken loginissa

--- app/(tabs)/Login.tsx
+++ app/(tabs)/Login.tsx
@@ -3,11 +3,31 @@ import {
   View,
   StyleSheet,
   ImageBackground,
+  Alert,
   ActivityIndicator,
 } from 'react-native';
 import Nappi from '@/components/Button';
+import {
+  GoogleSignin,
+  statusCodes,
+  SignInResponse,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 import BirthdayModal from '@/components/DatePicker';
-import GuestBirthdayModal from '@/components/BirthdayModal'; // Import the new modal
+
+// Configure Google Sign-In
+GoogleSignin.configure({
+  webClientId:
+    '946110492595-2boqsje3qba3aoj6uo3npvsooj8rrfp0.apps.googleusercontent.com',
+  offlineAccess: true,
+  scopes: [
+    'profile',
+    'email',
+    'openid',
+    'https://www.googleapis.com/auth/user.birthday.read',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ],
+});
 
 interface LoginScreenProps {
   navigation: any;
@@ -15,22 +35,84 @@ interface LoginScreenProps {
 
 const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
   const [isSigningIn, setIsSigningIn] = useState(false);
-  const [showModal, setShowModal] = useState(false); // Show Birthday Modal
-  const [showGuestModal, setShowGuestModal] = useState(false); // Show GuestBirthdayModal
+  const [showModal, setShowModal] = useState(false); // State for modal visibility
 
   const signInWithGoogle = async () => {
-    // Implement Google Sign-In if needed
-    console.log('Google Sign-In method should be implemented here');
+    if (isSigningIn) return;
+    setIsSigningIn(true);
+
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo: SignInResponse = await GoogleSignin.signIn();
+
+      if (userInfo.data) {
+        const { idToken, user } = userInfo.data;
+
+        if (idToken) {
+          const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+          const userCredential = await auth().signInWithCredential(
+            googleCredential
+          );
+          const firebaseUser = userCredential.user;
+
+          console.log('User signed in with Firebase:', firebaseUser);
+
+          const { accessToken } = await GoogleSignin.getTokens();
+
+          let userBirthday = null;
+
+          if (accessToken) {
+            const response = await fetch(
+              `https://people.googleapis.com/v1/people/me?personFields=birthdays`,
+              {
+                method: 'GET',
+                headers: {
+                  Authorization: `Bearer ${accessToken}`,
+                },
+              }
+            );
+
+            const data = await response.json();
+            console.log('Full People API Response:', data);
+
+            if (data && data.birthdays && data.birthdays.length > 0) {
+              const birthday = data.birthdays[0].date;
+              console.log('User Birthday:', birthday);
+              userBirthday = birthday;
+            } else {
+              console.log(
+                'Birthday is not available or not found in the response.'
+              );
+            }
+          } else {
+            console.log('Access token is not available.');
+          }
+
+          const givenName = user.givenName || 'Guest';
+          navigation.navigate('Menu', { givenName, userBirthday });
+        } else {
+          console.error('idToken is not available:', userInfo);
+          Alert.alert('Sign-In Error', 'No valid idToken returned.');
+        }
+      } else {
+        console.error('userInfo.data is null:', userInfo);
+        Alert.alert('Sign-In Error', 'No valid user information returned.');
+      }
+    } catch (error) {
+      console.error('Error during sign-in:', error);
+    } finally {
+      setIsSigningIn(false);
+    }
+  };
+
+  const navigateAsGuest = () => {
+    setShowModal(true); // Avataan birthday modaali kun kirjaudutaan vieraana
   };
 
   const handleSubmitBirthday = (birthday: { day: number; month: number }) => {
-    setShowModal(false); // Close Birthday Modal
-    navigation.navigate('Menu', { userBirthday: birthday });
-  };
-
-  const handleGuestBirthdaySubmit = () => {
-    setShowGuestModal(false); // Close GuestBirthdayModal
-    setShowModal(true); // Show BirthdayModal
+    const givenName = 'Guest';
+    setShowModal(false); // Kun pvm valittu, suljetaan modal
+    navigation.navigate('Menu', { givenName, userBirthday: birthday });
   };
 
   return (
@@ -45,23 +127,12 @@ const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
         ) : (
           <>
             <Nappi title="Login with Google" onPress={signInWithGoogle} />
-            <Nappi
-              title="Continue as a guest"
-              onPress={() => setShowGuestModal(true)}
-            />
+            <Nappi title="Continue as a guest" onPress={navigateAsGuest} />
           </>
         )}
       </View>
 
-      {/* Render GuestBirthdayModal and BirthdayModal */}
-      {showGuestModal && (
-        <GuestBirthdayModal
-          isVisible={showGuestModal}
-          onClose={() => setShowGuestModal(false)}
-          onSubmit={handleGuestBirthdaySubmit}
-        />
-      )}
-
+      {/* Render BirthdayModal if showModal is true */}
       {showModal && (
         <BirthdayModal
           isVisible={showModal}
